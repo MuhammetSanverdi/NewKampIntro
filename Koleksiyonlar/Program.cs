887491f9c2997ad3928dc75276b7a5fda53dd8b3
MuhammetSanverdi <77416025+MuhammetSanverdi@users.noreply.github.com>
2021-01-24 00:31:04 +0300

    23 ocak

--- Koleksiyonlar/Program.cs
+++ Koleksiyonlar/Program.cs
@@ -7,6 +7,7 @@ namespace Koleksiyonlar
     {
         static void Main(string[] args)
         {
+
             //string[] isimler = new string[] {"Engin", "Murat", "Kerem", "Halil" };
             //Console.WriteLine(isimler[0]);
             //Console.WriteLine(isimler[1]);
