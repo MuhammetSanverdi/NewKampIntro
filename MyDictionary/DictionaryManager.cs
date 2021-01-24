0d4165181b6fe9995429f870512f48b3199daca4
MuhammetSanverdi <77416025+MuhammetSanverdi@users.noreply.github.com>
2021-01-25 02:20:40 +0300

    ödev

---
+++ MyDictionary/DictionaryManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class DictionaryManager<TKey,TValue>
+    {
+        TKey[] kArray;
+        TValue[] vArray;
+        TKey[] tempKArray;
+        TValue[] tempVArray;
+
+        public DictionaryManager()
+        {
+            kArray = new TKey[0];
+            vArray = new TValue[0];
+        }
+        public void Add(TKey key,TValue value)
+        {
+            tempKArray = kArray;
+            tempVArray = vArray;
+
+            kArray = new TKey[kArray.Length + 1];
+            vArray = new TValue[vArray.Length + 1];
+            for (int i = 0; i < tempKArray.Length; i++)
+            {
+                kArray[i] = tempKArray[i];
+
+            }
+            for (int i = 0; i < tempVArray.Length; i++)
+            {
+                vArray[i] = tempVArray[i];
+            }
+            kArray[kArray.Length - 1] = key;
+            vArray[vArray.Length - 1] = value;
+        }
+        public TKey[] Item1
+        {
+            get { return kArray; }
+        }
+
+        public TValue[] Item2
+        {
+            get { return vArray; }
+
+        }
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DictionaryManager<int, string> sehirler = new DictionaryManager<int, string>();
+            sehirler.Add(1, "Adana");
+            sehirler.Add(2, "Adıyaman");
+            sehirler.Add(3, "Afyon");
+            sehirler.Add(4, "Ağrı");
+            sehirler.Add(5, "Amasya");
+            sehirler.Add(6, "Ankara");
+            sehirler.Add(7, "Antalya");
+            sehirler.Add(8, "Artvin");
+            sehirler.Add(9, "Aydın");
+
+
+            for (int i = 0; i < sehirler.Item1.Length; i++)
+            {
+                Console.WriteLine(sehirler.Item1[i] + " : " + sehirler.Item2[i]);
+
+            }
+
+        }
+    }
+}
