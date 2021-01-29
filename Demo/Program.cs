d2a33c86a52bfd4e80d746bd5dd14767ecae8dee
MuhammetSanverdi <77416025+MuhammetSanverdi@users.noreply.github.com>
2021-01-29 19:04:00 +0300

    C#

---
+++ Demo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.Update();
+        }
+    }
+    class CustomerManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Customer Added");
+
+        }
+        public void Update()
+        {
+            Console.WriteLine("Customer Updated");
+
+        }
+    }
+    class ProductManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Product Added");
+        }
+        public void Update()
+        {
+            Console.WriteLine("Product Updated");
+        }
+    }
+}
