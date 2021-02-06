884cfafa5d9a801b53415ec8b438890a6bf90e76
MuhammetSanverdi <77416025+MuhammetSanverdi@users.noreply.github.com>
2021-02-06 17:29:27 +0300

    C#

---
+++ EntityFrameworkDemo/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFrameworkDemo
+{
+    public class Product
+    {
+        public int ProductId { get; set; }//PrimaryKey
+        public int CategoryId { get; set; }//Foreign Key
+        public string ProductName { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public decimal UnitPrice { get; set; }
+        public short UnitsInStock { get; set; }
+        
+
+
+    }
+}
---
+++ EntityFrameworkDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EntityFrameworkDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ADO.NET
+            //Entity Framework -Bir ORM - Object relational mapping
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
